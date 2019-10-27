54f44afc891dfdf43d798fd4cce18f255f6e0427
Remy Machavoine <rmachavoine@wynd.eu>
2019-10-27 21:56:14 +0100

    upload button

--- resources/views/files/upload.blade.php
+++ resources/views/files/upload.blade.php
@@ -1,18 +1,29 @@
 @include ('layouts.header')
-<h1>Upload your File !</h1>
-
+<style type="text/css">
+    .container {
+        text-align: center;
+        margin: 10px;
+    }
+    .row {
+        flex: auto;
+        justify-content: center;
+    }
+</style>
 <div class="login-page">
-    <div class="container">
-        <div class="row">
-            <form action="/process" enctype="multipart/form-data" method="POST">
-                @csrf
-                <p>
-                    <label for="photo">
-                        <input type="file" name="photo" id="photo">
-                    </label>
-                </p>
-                <button>Upload</button>
-            </form>
+    <div class="col-md-10 offset-md-1">
+        <div class="container">
+            <h1>Upload your File !</h1>
+            <div class="row">
+                <form action="/upload" enctype="multipart/form-data" method="POST">
+                    @csrf
+                    <div class="input-group mb-3">
+                            <input type="file" name="photo" id="photo" class="form-control" aria-label="Recipient's username" aria-describedby="button-addon2">
+                        <div class="input-group-append">
+                            <button type="submit" class="btn btn-outline-primary" id="button-addon2">Upload</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
